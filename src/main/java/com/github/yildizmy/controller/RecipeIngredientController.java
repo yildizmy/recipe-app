df57cdc8bcefcc40c0e14ededb68abb813baf705
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-02-18 15:57:52 +0300

    refactor: shorten constant variable

--- src/main/java/com/github/yildizmy/controller/RecipeIngredientController.java
+++ src/main/java/com/github/yildizmy/controller/RecipeIngredientController.java
@@ -1,17 +1,18 @@
 package com.github.yildizmy.controller;
 
-import com.github.yildizmy.common.Constants;
+import com.github.yildizmy.dto.request.RecipeIngredientRequest;
 import com.github.yildizmy.dto.response.ApiResponse;
 import com.github.yildizmy.dto.response.CommandResponse;
-import lombok.RequiredArgsConstructor;
-import com.github.yildizmy.dto.request.RecipeIngredientRequest;
 import com.github.yildizmy.service.RecipeIngredientService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.Clock;
 import java.time.Instant;
+
+import static com.github.yildizmy.common.Constants.SUCCESS;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,7 +30,7 @@ public class RecipeIngredientController {
     @PostMapping("/recipeIngredients")
     public ResponseEntity<ApiResponse<CommandResponse>> addIngredientToRecipe(@Valid @RequestBody RecipeIngredientRequest request) {
         final CommandResponse response = recipeIngredientService.addIngredientToRecipe(request);
-        return ResponseEntity.ok(new ApiResponse<>(Instant.now(clock).toEpochMilli(), Constants.SUCCESS, response));
+        return ResponseEntity.ok(new ApiResponse<>(Instant.now(clock).toEpochMilli(), SUCCESS, response));
     }
 
     /**
@@ -40,6 +41,6 @@ public class RecipeIngredientController {
     @DeleteMapping("/recipeIngredients/recipes/{recipeId}/ingredients/{ingredientId}")
     public ResponseEntity<ApiResponse<CommandResponse>> removeIngredientFromRecipe(@PathVariable long recipeId, @PathVariable long ingredientId) {
         final CommandResponse response = recipeIngredientService.removeIngredientFromRecipe(recipeId, ingredientId);
-        return ResponseEntity.ok(new ApiResponse<>(Instant.now(clock).toEpochMilli(), Constants.SUCCESS, response));
+        return ResponseEntity.ok(new ApiResponse<>(Instant.now(clock).toEpochMilli(), SUCCESS, response));
     }
 }
