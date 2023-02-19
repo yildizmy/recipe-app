eb19242ba7a11c6ca925fe7da9aa8a6be8b0a9c9
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-02-20 00:36:42 +0300

    feat: implement datetime config to format date and datetime fields

---
+++ src/main/java/com/github/yildizmy/config/AppConfig.java
@@ -0,0 +1,25 @@
+package com.github.yildizmy.config;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class AppConfig {
+
+    private static final String dateFormat = "dd.MM.yyyy";
+    private static final String dateTimeFormat = "dd.MM.yyyy HH:mm:ss";
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat(dateTimeFormat);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+        };
+    }
+}
